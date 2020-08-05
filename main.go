1405ab8a4f2e5d6f03e44035659a69b27716cb83
aca <acadx0@gmail.com>
2020-08-05 22:20:11 +0900

    update docs

--- main.go
+++ main.go
@@ -19,6 +19,12 @@ type DebugLogger struct {
 func main() {
 	var connOpt []jsonrpc2.ConnOpt
 
+	// Setup a debug logger,
+	// You can send a log to client, but each clients implement different way to see / debug log.
+	// So we just use simple trick to debug server.
+	//
+	// $ nc -lp 3000
+	// Use netcat on the terminal like this, and this server would write log to it.
 	logConn, err := net.Dial("tcp", "localhost:3000")
 	var logger *log.Logger
 	if err == nil {
