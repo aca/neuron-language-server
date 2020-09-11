219783bf3b7fe85af89adf228aeeba3a9dc09084
aca <acadx0@gmail.com>
2020-09-11 22:38:53 +0900

    avoid creating .neuron directory

--- neuron/query.go
+++ neuron/query.go
@@ -6,6 +6,20 @@ import (
 )
 
 func Query(arg ...string) (*QueryResult, error) {
+	// TODO: to avoid creating .neuron directory
+	// This should be optional with flags
+	// path, err := os.Getwd()
+	// if err != nil {
+	// 	return nil, err
+	// }
+	//
+	// _, err = os.Stat(filepath.Join(path, ".neuron"))
+	// if os.IsNotExist(err) {
+	// 	return nil, nil
+	// } else if err != nil {
+	// 	return nil, err
+	// }
+
 	arg = append([]string{"query"}, arg...)
 	cmd := exec.Command("neuron", arg...)
 	output, err := cmd.Output()
